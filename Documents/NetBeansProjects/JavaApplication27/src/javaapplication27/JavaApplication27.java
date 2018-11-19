9827cae2b7bc62f4082312a0d2c1f4c0a6af5d8a
mahireddy99 <36803961+mahireddy99@users.noreply.github.com>
2018-11-19 10:49:11 -0500

    Update JavaApplication27.java

--- Documents/NetBeansProjects/JavaApplication27/src/javaapplication27/JavaApplication27.java
+++ Documents/NetBeansProjects/JavaApplication27/src/javaapplication27/JavaApplication27.java
@@ -1,83 +1,61 @@
-package javaapplication27;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication13;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class JavaApplication27 {
 
-    public static void displaywelcomemessage() {
-        System.out.println("PROJECT 02B ");
-        System.out.println("STUDENT ID :  1794805");
-        System.out.println("STUDENT NAME :  MAHENDER REDDY");
+public class JavaApplication13 {
+public static void printheader(){
+    System.out.println("**************");
+    System.out.println("draw triangle");
+    System.out.println("**************");
+}
+public static int askline(){
+    System.out.println("how many lines?  ");
+int line;
+Scanner sc= new Scanner(System.in);
+line = sc.nextInt();
+    return line;
+}
+public static String askcharacter()
+{
+    Scanner sc = new Scanner (System.in);
+     
+     String character;
+    System.out.println("which character to use  ");
+    character  =sc.nextLine();
+    return character;
+}
+public static void printline(int space,int charcount, String charToprint){
+    int i;
+    for(i=1;i<=space;i++){
+        System.out.print(" ");
     }
-
-    public static int asknumber(String question) {
-        int elements;
-        Scanner sc = new Scanner(System.in);
-        System.out.println(question);
-        elements = sc.nextInt();
-        return elements;
-
+    for (i=1;i<=charcount;i++){
+        System.out.print(charToprint);
     }
-
-    public static void fillwithRandom(int[] arrayvector, int elements) {
-        Random mn = new Random();
-        int i;
-        for (i = 0; i < elements; i++) {
-        arrayvector[i] = elements;
-
-        arrayvector[i] = mn.nextInt(6) + 5;
-
-        }
-    }
-
-    public static void displayarray(int[] arrayvector, int elements) {
-        
-       for (int i=0;i<elements;i++){
-           System.out.print("----");
-       }
-       
-      
-          System.out.println(" ");
-          System.out.print("|");
-        for (int i = 0; i < elements; i++) {
-        System.out.print(arrayvector[i] + " ");
-        System.out.print("|");
-        }
-        System.out.println(" ");
-      for (int i=0;i<elements;i++){
-           System.out.print("----");
-       }
-       
-        System.out.println(" ");
-
-    }
-
-    public static void displayTriangle(int arrayvector) {
-        int i;
-        int j;
-        for (i = 0; i < arrayvector; i++) {
-        for (j = 0; j <= i; j++) {
-        System.out.print("*");
-            }
-            System.out.println(" ");
-        }
-    }
-
+    System.out.println(" ");
+}
     public static void main(String[] args) {
-        displaywelcomemessage();
-
-        int elements = asknumber("how many elements? ");
-        int[] arrayvector = new int[elements];
-
-        fillwithRandom(arrayvector, elements);
-        displayarray(arrayvector, elements);
-
-        for (int i = 0; i < elements; i++) {
-        System.out.println("Triangle " + (i + 1));
-        displayTriangle(arrayvector[i]);
-        }
-
+       int i;
+       int line;
+       int space ;
+       int charcount ;
+       String charToprint;
+       line=askline();
+    String character = askcharacter();
+    for(i=0;i<=line;i++){
+        space =line-i;
+        charcount=(i*2)-1;
+       printline(space, charcount, character);
+        
+      
     }
-
+       
+    }
+    
 }
